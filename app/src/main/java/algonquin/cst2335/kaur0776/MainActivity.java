778262a950f6b8367ef2cf138a3b1f6deeba9e4c
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-09-18 16:58:30 -0400

    Commit 5 Removed the extra lines

--- app/src/main/java/algonquin/cst2335/kaur0776/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kaur0776/MainActivity.java
@@ -3,24 +3,23 @@ package algonquin.cst2335.kaur0776;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-
-
-
-
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.my_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
+
