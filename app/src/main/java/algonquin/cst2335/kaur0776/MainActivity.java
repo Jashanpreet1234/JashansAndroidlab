488066f9d7fd6ca191d9614a8cb2124b8aad7a8f
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-09-18 00:25:41 -0400

    Commit 5 Removed the extra lines

--- app/src/main/java/algonquin/cst2335/kaur0776/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kaur0776/MainActivity.java
@@ -6,6 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
