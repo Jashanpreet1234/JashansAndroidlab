a64480b021d24966817f3a73d83de7f669249eb3
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-09-19 12:06:31 -0400

    Commit 5 Removed the extra lines

--- app/src/main/java/algonquin/cst2335/kaur0776/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kaur0776/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +14,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+
+
+//Variables declarations
+        Button btn = findViewById(R.id.mybutton);
+        TextView mytext = findViewById(R.id.textview);
+      EditText myedit = findViewById(R.id.myedittext);
+
+
+//
+String editString = myedit.getText().toString();
+
+mytext.setText("Your edit text has:" + editString);
+
 
         final Button button = findViewById(R.id.my_button);
         button.setOnClickListener(new View.OnClickListener() {
